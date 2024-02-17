749617321f4dd44845a869520e15299391a8dbea
Smit <smitmpatil123@gmail.com>
2024-02-17 12:46:22 +0530

    App Setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -13,6 +14,11 @@ app.use(cors(
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
+
+const userRoutes = require("./routes/user.routes");
+
+app.use("/api/v1/user", userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`⚙️ Server is running on port ${process.env.PORT}`);
