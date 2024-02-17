24ca83e906cf801cbf1c34afae1c16f8f6045c38
Smit <smitmpatil123@gmail.com>
2024-02-17 11:18:27 +0530

    App Setup

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.json());
 app.use(cors(
     {
-        origin: `${process.env.CLIENT_URL}`
+        origin: `${process.env.CLIENT_URL}`,
+        credentials: true
     }
 ));
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
 
 app.listen(process.env.PORT, () => {
     console.log(`⚙️ Server is running on port ${process.env.PORT}`);
