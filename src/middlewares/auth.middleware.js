785a9c418f6d9b02dcc7b4fc7b895f20dcea76d8
Smit <smitmpatil123@gmail.com>
2024-02-18 13:20:30 +0530

    Verifying User through Auth Middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const User = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+
+const verifyJWT = async (req, res, next) => {
+    try {
+
+        const token = req.cookies?.accessToken
+
+        if (!token) return res.status(401).json({ message: "Unauthorized Request" })
+
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+        if (!user) return res.status(401).json({ message: "Invalid Access Token" })
+
+        req.user = user
+
+        next()
+
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+module.exports = verifyJWT
