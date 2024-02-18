180a3391e8a139c9d9f00d949008b3e1ca82a1dd
Smit <smitmpatil123@gmail.com>
2024-02-18 13:14:20 +0530

    Created User routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const upload = require("../middlewares/multer.middleware")
 
-const { register, login } = require("../controllers/user.controllers")
+const { register, login, logout } = require("../controllers/user.controllers")
+const verifyJWT = require("../middlewares/auth.middleware")
 
 router.post("/register",
     upload.single("avatar"),
     register
 )
 router.post("/login", login)
+router.post("/logout", verifyJWT, logout)
 
 module.exports = router;
