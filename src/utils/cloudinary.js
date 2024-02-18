2cf0e988839eae8bcce0048f220bd3fd7d27ab4a
Smit <smitmpatil123@gmail.com>
2024-02-18 15:18:46 +0530

    Image upload to Cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+const cloudinary = require("cloudinary");
+const fs = require("fs");
+
+cloudinary.v2.config({
+    cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`,
+    api_key: `${process.env.CLOUDINARY_API_KEY}`,
+    api_secret: `${process.env.CLOUDINARY_API_SECRET}`
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+
+        if (!localFilePath) return null
+
+        const response = await cloudinary.v2.uploader.upload(localFilePath, {
+
+            resource_type: "auto"
+
+        })
+
+        fs.unlinkSync(localFilePath)
+
+        return response.secure_url
+
+    }
+    catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+module.exports = uploadOnCloudinary
